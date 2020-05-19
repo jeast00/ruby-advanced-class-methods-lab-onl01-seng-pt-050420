bc2fa03a8f6838e0828356dfc7e1d415cab0b8aa
Jonathan East <jonathan_east@ymail.com>
2020-05-19 19:20:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,7 +46,7 @@ class Song
 
   def self.new_from_filename(file_name)
     song = self.new
-    song.name = name.split(" - ")[1].sub(/\.[^.]+\z/, '')
+    song.name = name.split(" - ").sub(/\.[^.]+\z/, '')[1]
     artist.name = name.split(" - ")[0]
     song
   end
