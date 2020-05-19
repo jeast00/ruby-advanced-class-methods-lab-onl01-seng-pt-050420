f77f2a9e3ed3a8c78dfb57f82626017f9e969c2a
Jonathan East <jonathan_east@ymail.com>
2020-05-19 19:39:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,15 +46,15 @@ class Song
 
   def self.new_from_filename(name)
     song = self.new
-    song.name = name.split(" - ")[1].sub(/\.[^.]+\z/, '')
     song.artist_name = name.split(" - ")[0]
+    song.name = name.split(" - ")[1].sub(/\.[^.]+\z/, '')
     song
   end
 
   def self.create_from_filename(name)
     song = self.new
-    song.name = name.split(" - ")[1].sub(/\.[^.]+\z/, '')
     song.artist_name = name.split(" - ")[0]
+    song.name = name.split(" - ")[1].sub(/\.[^.]+\z/, '')
     @@all << song
     song
   end
